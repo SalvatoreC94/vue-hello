e78ec0baffa3939c71796bdc93d4abfcec33e99a
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-08-28 13:51:50 +0200

    Hello Vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Js Cololegato')
+const { createApp } = Vue
+
+// Creazione dell'app Vue
+createApp({
+    // Definizione dei dati utilizzando l'Option API
+    data() {
+        return {
+            // Messaggio da visualizzare nel div con id "app"
+            message: 'Hello Vue!'
+        }
+    }
+    // Monta l'app Vue sull'elemento con id "app"
+}).mount('#app')
